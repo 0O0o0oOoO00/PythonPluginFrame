a8fade1ef8e66117423f489b3a957fee2c6e7481
LA-DI-DA <2106423460@QQ.COM>
2022-07-27 23:09:30 +0800

    Add files via upload

--- utils/PluginTools/utils.py
+++ utils/PluginTools/utils.py
@@ -2,7 +2,8 @@ from importlib.resources import Package
 import importlib
 from typing import Dict, List, Union, ClassVar
 from .resources import Parameter
-from ..CommandTools.utils import InputCommand, printHelp
+from ..CommandTools.resources import InputCommand
+from ..CommandTools.utils import printHelp
 
 
 def listParameters(pluginMain: ClassVar):
@@ -18,30 +19,6 @@ def getParametersDict(pluginMain: ClassVar) -> Dict[str, Parameter]:
 	return parametersDict
 
 
-# def setParameter(plugin: Package, parameter: str, value: Union[int, float, bool, str]):
-# 	while True:
-# 		commandStr = input("\n>> ")
-# 		if commandStr:
-# 			inputCommand = InputCommand(commandStr)
-# 			if inputCommand.command == "set" and hasattr(inputCommand, "parameter") and hasattr(inputCommand, "value"):
-# 				parameter, value = inputCommand.parameter, inputCommand.value
-# 				parametersDict = dict()
-# 				for i in plugin.parametersList():
-# 					parametersDict[i.name] = i
-# 				setattr(plugin, parameter, parametersDict[parameter].type_(value))
-# 			elif inputCommand.command == "list":
-# 				listParameters(plugin)
-# 			elif inputCommand.command == "help":
-# 				printHelp("help", None, "Show this help message.")
-# 				printHelp("set", "set [PARAMETER] [VALUE]", "Set the plugin's parameter.")
-# 				printHelp("list", None, "List plugin parameters.")
-# 				printHelp("quit", None, "Quit.")
-# 				printHelp("exit", None, "Exit.")
-# 			elif inputCommand.command == "run":
-# 				plugin.run()
-# 				break
-# 			elif inputCommand.command == "quit" or inputCommand.command == "exit":
-# 				break
 def setParameter(pluginMain: ClassVar, parameter: str, value: Union[int, float, bool, str]):
 	parametersDict = getParametersDict(pluginMain)
 	setattr(pluginMain, parameter, parametersDict[parameter].type_(value))
