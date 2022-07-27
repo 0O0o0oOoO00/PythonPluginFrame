e1af451ed554732c9394b146de4bbc5b30d00b69
LA-DI-DA <2106423460@QQ.COM>
2022-07-27 23:00:19 +0800

    Delete bin directory

--- bin/main.py
+++
@@ -1,8 +0,0 @@
-import click
-# from commandLine import BilibiliTool_CommandLine
-# from noCommandLine import BilibiliTool_noCommandLine
-
-if __name__ == "__main__":
-	# BilibiliTool()
-	# BilibiliTool_CommandLine(["-c"])
-	pass
