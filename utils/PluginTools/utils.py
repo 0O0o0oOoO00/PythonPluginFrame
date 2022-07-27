69ecca7ea6638a3f0c84377c17626474de6c2f2e
LA-DI-DA <2106423460@QQ.COM>
2022-07-27 23:11:29 +0800

    Update utils.py

--- utils/PluginTools/utils.py
+++ utils/PluginTools/utils.py
@@ -1,25 +1,23 @@
 from importlib.resources import Package
 import importlib
-from typing import Dict, List, Union, ClassVar
+from typing import Dict, Union
 from .resources import Parameter
-from ..CommandTools.resources import InputCommand
-from ..CommandTools.utils import printHelp
 
 
-def listParameters(pluginMain: ClassVar):
+def listParameters(pluginMain: object):
 	print(f"Name\t\t|\tType\t|\tMust\t|\tNowValue\t|\tDescription")
 	for i, parameter in enumerate(pluginMain.parametersList()):
 		print(f"{parameter.name}\t|\t{parameter.type_.__name__}\t|\t{parameter.must}\t|\t{getattr(pluginMain, parameter.name)}\t\t|\t{parameter.description}")
 
 
-def getParametersDict(pluginMain: ClassVar) -> Dict[str, Parameter]:
+def getParametersDict(pluginMain: object) -> Dict[str, Parameter]:
 	parametersDict = dict()
 	for parameter in pluginMain.parametersList():
 		parametersDict[parameter.name] = parameter
 	return parametersDict
 
 
-def setParameter(pluginMain: ClassVar, parameter: str, value: Union[int, float, bool, str]):
+def setParameter(pluginMain: object, parameter: str, value: Union[int, float, bool, str]):
 	parametersDict = getParametersDict(pluginMain)
 	setattr(pluginMain, parameter, parametersDict[parameter].type_(value))
 
@@ -28,9 +26,9 @@ def loadPlugin(pluginName: str) -> Package:
 	return importlib.import_module(f"PluginFrame.plugins.{pluginName}.run")
 
 
-def loadPluginMain(plugin: Package) -> ClassVar:
+def loadPluginMain(plugin: Package) -> object:
 	return plugin.Main()
 
 
-def runPlugin(pluginMian: ClassVar):
+def runPlugin(pluginMian: object):
 	pluginMian.run()
