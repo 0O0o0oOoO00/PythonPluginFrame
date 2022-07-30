e83abf2e7a5eb7dd5563f670365bd2c99d5134c8
LA-DI-DA <2106423460@QQ.COM>
2022-07-30 15:17:25 +0800

    Update utils.py

--- utils/PluginTools/utils.py
+++ utils/PluginTools/utils.py
@@ -27,24 +27,22 @@ def getAllPlugin() -> List[str]:
 
 
 def listParameters(pluginMain: object):
-	# print("{0:10}\t|\t{1:5}\t|\t{2:5}\t|\t{3:15}\t|\t{4:}".format("Name", "Type", "Must", "CurrentValue", "Description"))
 	content = list()
-	for i, parameter in enumerate(pluginMain.parametersList()):
-		# print(f"\t{parameter.name}\n\t\t[Type]: {parameter.type_.__name__}\n\t\t[Must]: {parameter.must}\n\t\t[CurrentValue]: {getattr(pluginMain, parameter.name)}\n\t\t[Description]: {parameter.description}")
-		# print("{0:10}\t|\t{1:5}\t|\t{2:5}\t|\t{3:15}\t|\t{4:}".format(str(parameter.name), str(parameter.type_.__name__), str(parameter.must), str(getattr(pluginMain, parameter.name), str(parameter.description))))
+	for i, parameter in enumerate(pluginMain.parametersList):
 		content.append([parameter.name, parameter.type_.__name__, parameter.must, getattr(pluginMain, parameter.name), parameter.description])
 	print(tabulate(content, headers=["Name", "Type", "Must", "CurrentValue", "Description"]))
 
 
 def getParametersDict(pluginMain: object) -> Dict[str, Parameter]:
 	parametersDict = dict()
-	for parameter in pluginMain.parametersList():
+	for parameter in pluginMain.parametersList:
 		parametersDict[parameter.name] = parameter
 	return parametersDict
 
 
-def setParameter(pluginMain: object, parameter: str, value: Union[int, float, bool, str]):
-	parametersDict = getParametersDict(pluginMain)
+def setParameter(pluginMain: object, parameter: str, value: Union[int, float, bool, str], parametersDict: Union[Dict[str, object], None] = None):
+	if parametersDict is None:
+		parametersDict = getParametersDict(pluginMain)
 	setattr(pluginMain, parameter, parametersDict[parameter].type_(value))
 
 
